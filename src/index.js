34a63a55384883b0560b130df53691479b72feee
NatiG <nati.g5684@gmail.com>
2022-03-02 13:11:22 +0300

    Add styles

--- src/index.js
+++ src/index.js
@@ -1,2 +1,29 @@
 import './style.css';
-document.body.append(document.createElement('h2').textContent = 'hello')
+
+const ul = document.querySelector('ul');
+
+const chores = [
+    {
+        description: 'wash dishes',
+        completed: false,
+        index: 1,
+    },
+    {
+        description: 'fix the car',
+        completed: false,
+        index: 2,
+    },
+    {
+        description: 'clean bedroom',
+        completed: false,
+        index: 3,
+    }
+];
+
+const list = () => {
+    for (let i = 0; i < chores.length; i += 1) {
+        ul.innerHTML += `<li><input type="checkbox" id="chores"><label for="chores">${chores[i].description}</label></li>`;
+      }
+  };
+
+list();
