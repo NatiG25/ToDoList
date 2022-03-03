881ae349f8df47644a97a77f29bf068dc439b1ba
NatiG <nati.g5684@gmail.com>
2022-03-03 19:15:09 +0300

    Add Addtasks function

--- src/index.js
+++ src/index.js
@@ -1,29 +1,39 @@
 import './style.css';
 
 const ul = document.querySelector('ul');
+let input = document.querySelector('.inputAdd');
 
-const chores = [
-  {
-    description: '',
-    completed,
-    index,
-  },
-  {
-    description: '',
-    completed,
-    index,
-  },
-  {
-    description: '',
-    completed,
-    index,
-  },
-];
-
-const list = () => {
-  for (let i = 0; i < chores.length; i += 1) {
-    ul.innerHTML += `<li><input type="checkbox" id="chores"><label for="chores">${chores[i].description}</label></li>`;
+class TaskObj {
+  constructor(description, index) {
+    this.description = description;
+    this.completed = false;
+    this.index = index;
   }
+}
+
+const parseTasks = JSON.parse(localStorage.getItem('tasks'));
+const Localtasks = parseTasks || [];
+
+const populateStorage = () => {
+  localStorage.setItem('tasks', JSON.stringify(Localtasks));
 };
 
-list();
+let itemsInsertion = () => {
+  ul.innerHTML = '';
+  for(let i = 0; i < Localtasks.length; i += 1) {
+    ul.innerHTML += `<li><input type="checkbox"><input value="${Localtasks[i].description}" class="item-input"> <span id="${Localtasks[i].index - 1}" class="trash-icon">&#x1f5d1;</span></li>`;
+  }
+}
+
+const Addtasks = () => {
+  input.addEventListener('keypress', (e) => {
+    if(e.key === 'Enter'){
+      const NewObj = new TaskObj(input.value, Localtasks.length +1);
+      Localtasks.splice(Localtasks.length, 0, NewObj);
+      itemsInsertion();
+      populateStorage();
+      input.value = '';
+    }
+  })
+}
+Addtasks();
