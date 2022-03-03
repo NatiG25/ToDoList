4f1612c5e7a7d92e65ffc2ede15ed21330fa430b
NatiG <nati.g5684@gmail.com>
2022-03-04 01:30:58 +0300

    Add remove function

--- src/index.js
+++ src/index.js
@@ -18,10 +18,10 @@ const populateStorage = () => {
   localStorage.setItem('tasks', JSON.stringify(Localtasks));
 };
 
-let itemsInsertion = () => {
+const itemsInsertion = () => {
   ul.innerHTML = '';
   for(let i = 0; i < Localtasks.length; i += 1) {
-    ul.innerHTML += `<li><input type="checkbox"><input value="${Localtasks[i].description}" class="item-input"> <span id="${Localtasks[i].index - 1}" class="trash-icon">&#x1f5d1;</span></li>`;
+    ul.innerHTML += `<li><input type="checkbox"><input value="${Localtasks[i].description}" class="item-input"> <span id= "${Localtasks[i].index - 1}" class="trash-icon"> &#x1f5d1; </span></li>`;
   }
 }
 
@@ -36,4 +36,46 @@ const Addtasks = () => {
     }
   })
 }
-Addtasks();
+
+const removeTasks = (e) => {
+  if(e.target.tagName === 'SPAN') {
+    const element = e.target;
+    const elementId = element.id;
+    element.parentNode.remove();
+    Localtasks.splice(elementId, 1);
+    Localtasks.forEach((elem) => {
+      if (elem.index > elementId) {
+        elem.index -= 1;
+      }
+    });
+    itemsInsertion();
+    populateStorage();
+  
+  }
+}
+
+ul.addEventListener('click', removeTasks);
+
+const editTask = (e) => {
+  if (e.target.classList('item-input'))
+  const element = e.target;
+  const targetID = parseInt(element.nextElementSibling.id, 10) + 1;
+  Localtasks.forEach(elem => {
+    if(elem.index === targetID) {
+      elem.description === element.value;
+    }
+  })
+  itemsInsertion();
+  populateStorage();
+}
+
+ul.addEventListener('focusout', editTask);
+
+const DisplayLocalTasks = () => {
+  if(Localtasks.length !== 0) {
+    itemsInsertion();
+  }
+  Addtasks();
+};
+
+DisplayLocalTasks();
