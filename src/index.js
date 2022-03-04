b39e2f3e4e6d5c3a7202c49ca827c912e6cd6249
NatiG <nati.g5684@gmail.com>
2022-03-04 16:30:50 +0300

    Removed all modules

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import './style.css';
-import removeTasks from './modules/remove';
-import editTask from './modules/edit';
-import DisplayLocalTasks from './modules/displayLocalTasks';
 
 const ul = document.querySelector('ul');
-export const input = document.querySelector('.inputAdd');
+const input = document.querySelector('.inputAdd');
 
-export class TaskObj {
+class TaskObj {
   constructor(description, index) {
     this.description = description;
     this.completed = false;
@@ -15,21 +12,70 @@ export class TaskObj {
 }
 
 const parseTasks = JSON.parse(localStorage.getItem('tasks'));
-export const Localtasks = parseTasks || [];
+const Localtasks = parseTasks || [];
 
-export const populateStorage = () => {
+const populateStorage = () => {
   localStorage.setItem('tasks', JSON.stringify(Localtasks));
 };
 
-export const itemsInsertion = () => {
+const itemsInsertion = () => {
   ul.innerHTML = '';
-  for(let i = 0; i < Localtasks.length; i += 1) {
+  for (let i = 0; i < Localtasks.length; i += 1) {
     ul.innerHTML += `<li><input type="checkbox"><input value="${Localtasks[i].description}" id="item-input"> <span id= "${Localtasks[i].index - 1}" class="trash-icon"> &#x1f5d1; </span></li>`;
   }
-}
+};
+
+const Addtasks = () => {
+  input.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+      const NewObj = new TaskObj(input.value, Localtasks.length + 1);
+      Localtasks.splice(Localtasks.length, 0, NewObj);
+      itemsInsertion();
+      populateStorage();
+      input.value = '';
+    }
+  });
+};
+
+const removeTasks = (e) => {
+  if (e.target.tagName === 'SPAN') {
+    const element = e.target;
+    const elementId = element.id;
+    element.parentNode.remove();
+    Localtasks.splice(elementId, 1);
+    Localtasks.forEach((elem) => {
+      if (elem.index > elementId) {
+        elem.index -= 1;
+      }
+    });
+    itemsInsertion();
+    populateStorage();
+  }
+};
 
 ul.addEventListener('click', removeTasks);
 
+const editTask = (e) => {
+  if (e.target.id === 'item-input') {
+    const element = e.target;
+    const targetID = parseInt(element.nextElementSibling.id, 10) + 1;
+    Localtasks.forEach((elem) => {
+      if (elem.index === targetID) {
+        elem.description = element.value;
+      }
+    });
+    itemsInsertion();
+    populateStorage();
+  }
+};
+
 ul.addEventListener('focusout', editTask);
+
+const DisplayLocalTasks = () => {
+  if (Localtasks.length !== 0) {
+    itemsInsertion();
+  }
+  Addtasks();
+};
 
 DisplayLocalTasks();
